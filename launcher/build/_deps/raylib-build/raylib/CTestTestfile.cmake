2a8c8a7c4521ff32c482d9f5c1c061db2bb9090d
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-08-29 15:21:44 +0900

    add l

--- launcher/build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/riookabe/Desktop/nyanthu/nyanthu - game/nyanthu_game/setter/build/_deps/raylib-src/src
-# Build directory: /Users/riookabe/Desktop/nyanthu/nyanthu - game/nyanthu_game/setter/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
